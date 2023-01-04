93d284f023ec79f6b19de8d1d295e159db3e1800
Arthur Generino Calado Silva <88391563+Arthur1256DEV@users.noreply.github.com>
2023-01-04 17:50:41 -0300

    Initial Commit

---
+++ java.js
@@ -0,0 +1,106 @@
+function addStyle() {
+    const navigation = new CSSStyleSheet();
+    const navigationLi = new CSSStyleSheet();
+    const logoOff = new CSSStyleSheet();
+    const search = new CSSStyleSheet();
+    const button = new CSSStyleSheet();
+    const button2 = new CSSStyleSheet();
+    const paragraph = new CSSStyleSheet();
+    const avatarOff = new CSSStyleSheet();
+    const Exiticon = new CSSStyleSheet();
+
+    navigation.insertRule(`.navigation {
+        width: 10vw;
+    }`);
+    
+    navigationLi.insertRule(`.list{
+        width: 50px;
+        height: 50px;
+    }`)
+  
+    logoOff.insertRule(`.logoimg {
+        visibility: hidden;
+        display: none;
+    }`);
+    
+    search.insertRule(`.iconSch{
+        width: 40px;
+    }`);
+
+    button.insertRule(`.iconMenu{
+        margin-left: 5vw;
+        display: none;
+    }`);
+
+    button2.insertRule(`.iconMenu2{
+        display: initial;
+        margin-left: 5vw;
+    }`);
+
+    paragraph.insertRule(`p{
+        display: none;
+        visibility: hidden;
+    }`);
+
+    Exiticon.insertRule(`.iconExit{
+        justify-content: center;
+        padding: 0;
+        width: 5vw;
+        height: 10vh;
+        flex-direction: row;
+    }`)
+
+    avatarOff.insertRule(`#avatar{
+        display: none;
+    }`)
+    document.adoptedStyleSheets = [navigation, navigationLi, logoOff, search, button, paragraph,  avatarOff, Exiticon, button2]
+    
+    
+
+}
+
+
+function addStyle2() {
+    const navigation = new CSSStyleSheet();
+    const logoOff = new CSSStyleSheet();
+    const search = new CSSStyleSheet();
+    const button = new CSSStyleSheet();
+    const paragraph = new CSSStyleSheet();
+    const avatarOff = new CSSStyleSheet();
+    const Exiticon = new CSSStyleSheet();
+
+    navigation.insertRule(`.navigation {
+        width: 40vw;
+    }`);
+    
+    logoOff.insertRule(`.logoimg {
+        visibility: initial;
+        display: flex;
+    }`);
+    
+    search.insertRule(`.iconSch{
+        width: 30vw;
+    }`);
+
+    button.insertRule(`.iconMenu{
+        margin-left: 1vw;
+    }`);
+
+    paragraph.insertRule(`p{
+        display: initial;
+        visibility: initial;
+    }`);
+
+    Exiticon.insertRule(`.iconExit{
+        justify-content: space-between;
+        padding: 7px;
+        width: 30vw;
+        height: 75px;
+        flex-direction: row-reverse;
+    }`)
+
+    avatarOff.insertRule(`#avatar{
+        display: initial;
+    }`)
+    document.adoptedStyleSheets = [navigation, logoOff, search, button, paragraph, avatarOff, Exiticon]
+}
